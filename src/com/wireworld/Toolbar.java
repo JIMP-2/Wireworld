c41bf59a07cd6284d3eadb2dbf7cc3589f7738e7
mjakubowska <martynajakubowska05@gmail.com>
2020-05-19 16:27:10 +0200

    dodanie mozliwosci wyboru liczby generacji

--- src/com/wireworld/Toolbar.java
+++ src/com/wireworld/Toolbar.java
@@ -1,5 +1,8 @@
 package com.wireworld;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
@@ -14,11 +17,14 @@ import com.wireworld.model.StandardRule;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.ToolBar;
+import javafx.util.Duration;
 
 
 public class Toolbar extends ToolBar {
 
     private GridView gridView;
+    private int generations;
+    private TextField genField;
 
 
 
@@ -42,20 +48,49 @@ public class Toolbar extends ToolBar {
         Button reset = new Button("Reset");
         reset.setOnAction(this::handleReset);
         Button start = new Button("Start");
-        start.setOnAction(this::handleStart);
+        start.setOnAction(actionEvent -> {
+            try {
+                handleStart(actionEvent);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
         Button stop = new Button("Stop");
         stop.setOnAction(this::handleStop);
 
-        this.getItems().addAll(drawEmpty, drawConductor,drawHead,drawTail, reset, step, start, stop, clean);
+        genField = new TextField("20");
+        Label label = new Label ("Liczba generacji: ");
+
+        this.getItems().addAll(drawEmpty, drawConductor,drawHead,drawTail, reset, step, label, genField, start, stop, clean);
     }
 
     private void handleStop(ActionEvent actionEvent) {
         this.simulator.stop();
     }
 
-    private void handleStart(ActionEvent actionEvent) {
-        switchToSimulatingState();
-        this.simulator.start();
+    private void handleStart(ActionEvent actionEvent) throws InterruptedException {
+        generations = Integer.parseInt(genField.getText());
+        Timeline pauseMaker = new Timeline(new KeyFrame(Duration.seconds(0.5), new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                switchToSimulatingState();
+                gridView.getSimulation().step();
+                gridView.draw();
+            }
+        }));
+        pauseMaker.setCycleCount(generations);
+        pauseMaker.play();
+        /*
+        for(int i=0; i<generations; i++) {
+            switchToSimulatingState();
+            this.gridView.getSimulation().step();
+            this.gridView.draw();
+            //Thread.sleep(1000);
+            System.out.println(i);
+
+
+        }
+*/
     }
 
 
